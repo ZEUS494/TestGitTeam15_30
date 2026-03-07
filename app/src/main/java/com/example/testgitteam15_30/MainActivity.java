e335151224d81155cedef2a2b5067c9e0c817d75
ddexus <dexusgod003@gmail.com>
2026-03-07 18:25:25 +0300

    Имя фамилия кнопка отправки

--- app/src/main/java/com/example/testgitteam15_30/MainActivity.java
+++ app/src/main/java/com/example/testgitteam15_30/MainActivity.java
@@ -2,23 +2,37 @@ package com.example.testgitteam15_30;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText nameText;
+    private EditText surnameText;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        nameText = findViewById(R.id.nameText);
+        surnameText = findViewById(R.id.surnameText);
+        sendButton = findViewById(R.id.sendButton);
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intent.putExtra("key_name", nameText.toString());
+                intent.putExtra("key_surname", surnameText.toString());
+
+                startActivity(intent);
+            }
         });
     }
 }
